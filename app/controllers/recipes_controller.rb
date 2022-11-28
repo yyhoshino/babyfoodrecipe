4b1ff6dbb9f40f9f3e45b80be7ea115df2f05355
yyhoshino <rozary35@gmail.com>
2022-11-29 01:43:41 +0900

    投稿機能

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,22 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:dish_name, :ingredient, :make, :introduction, :image)
+  end
 end
