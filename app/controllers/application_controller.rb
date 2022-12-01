28e981f35c8c34c52400dbddd9fdb1ca4834e14c
yyhoshino <rozary35@gmail.com>
2022-12-01 22:43:00 +0900

    検索フォームの設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  end
+
+  def set_search
+    @q = Recipe.ransack(params[:q])
+    @recipes = @q.result
   end
 
 end
