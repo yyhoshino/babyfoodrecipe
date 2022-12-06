5f9c8fd041d65601fff06f520edd1c582c8b6eb1
yyhoshino <rozary35@gmail.com>
2022-12-06 16:57:34 +0900

    メインページのお気に入り機能の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,12 @@ class FavoritesController < ApplicationController
   def create
     @recipe_favorite = Favorite.new(user_id: current_user.id, recipe_id: params[:recipe_id])
     @recipe_favorite.save
-    redirect_to recipe_path(params[:recipe_id]) 
+    redirect_back(fallback_location: root_path )
   end
   
   def destroy
     @recipe_favorite = Favorite.find_by(user_id: current_user.id, recipe_id: params[:recipe_id])
     @recipe_favorite.destroy
-    redirect_to recipe_path(params[:recipe_id]) 
+    redirect_back(fallback_location: root_path ) 
   end
 end
