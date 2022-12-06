4897c65e0f411785c8b4956a4276fd7fef6a96a4
yyhoshino <rozary35@gmail.com>
2022-12-06 17:31:27 +0900

    お気に入りページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @recipes = @user.recipes
   end
+
+  def favorites
+    @user = User.find(params[:id])
+    favorites= Favorite.where(user_id: @user.id).pluck(:recipe_id)
+    @favorite_recipes = Recipe.find(favorites)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-  resources :users, only: :show
+  resources :users, only: :show do
+    member do
+      get :favorites
+    end
+  end
 end
