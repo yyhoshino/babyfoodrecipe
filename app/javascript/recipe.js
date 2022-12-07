eec299853a8b4f29e83080544c922dcfce30eebf
yyhoshino <rozary35@gmail.com>
2022-12-07 10:04:02 +0900

    メイン画面の投稿に動きをつける

---
+++ app/javascript/recipe.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function(){
+  const recipes = document.querySelectorAll(".recipe-content");
+  recipes.forEach(function(recipe) {
+    recipe.addEventListener('mouseover', function() {
+      this.setAttribute("style", "box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);");
+    });
+    recipe.addEventListener('mouseout', function() {
+      this.removeAttribute("style", "box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.5);");
+    });
+  });
+});
