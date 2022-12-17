c491b027779677a482256b2913b2f6442b5b1563
yyhoshino <rozary35@gmail.com>
2022-12-17 22:46:11 +0900

    comenntsコントローラーの作成、ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to:"recipes#index"
   resources :recipes do
     resource :favorites, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
     collection do
       get 'search'
     end
