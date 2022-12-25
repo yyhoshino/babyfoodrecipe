e90db3576cf58ad9e9be063c5481373039c1a6a6
yyhoshino <rozary35@gmail.com>
2022-12-25 22:47:48 +0900

    コメント削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,24 @@
 class CommentsController < ApplicationController
   def create
+    @recipe = Recipe.find(params[:recipe_id])
     @comment = Comment.new(comment_params)
-      if @comment.save
-        redirect_to recipe_path(@comment.recipe)
-      else
-        @recipe = @comment.recipe
-        @comments = @recipe.comments
-        render "recipes/show"
-      end
+    @comment.user_id = current_user.id
+    @comment.recipe_id = @recipe.id
+    if @comment.save
+      redirect_to recipe_path(@recipe)
+    end
   end
 
   def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_comments = @recipe.comments
+    if Comment.find_by(id: params[:id], recipe_id: params[:recipe_id]).destroy
+      redirect_to recipe_path(@recipe)
+    end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id, recipe_id: params[:recipe_id])
+    params.require(:comment).permit(:content)
   end
 end
