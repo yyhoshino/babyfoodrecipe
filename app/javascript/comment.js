fe42faec832936ee6292e002684c3e225d3e88c3
yyhoshino <rozary35@gmail.com>
2023-01-04 23:06:38 +0900

    turbolinks解除

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,7 +1,12 @@
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", () => {
-    console.log("イベント発火");
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/recipes", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
   });
 };
 
