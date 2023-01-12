009ca5514cd4409641a68fd0d00cecaccb315026
yyhoshino <rozary35@gmail.com>
2023-01-12 23:47:56 +0900

    jQueryの読み込み

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 require("../recipe");
 require("../comment")
-
+//= require jquery
+//= require rails-ujs
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
